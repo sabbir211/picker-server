42c27681866aef681d174fb29c1735d51e687cb5
sabbir <sabbir21122@gmail.com>
2022-05-25 14:42:46 +0600

    order placed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require("cors")
@@ -10,34 +11,49 @@ app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Picker server Running Well")
+
 })
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.izp32.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
- await client.connect()
- const toolsCollection=client.db("tools").collection("tools")
- const usersCollection=client.db("users").collection("users")
+        await client.connect()
+        const toolsCollection = client.db("tools").collection("tools")
+        const usersCollection = client.db("users").collection("users")
+        const ordersCollection = client.db("orders").collection("orders")
 
- app.get("/tools",async(req,res)=>{
-     const result=await toolsCollection.find().toArray()
-     res.send(result)
- })
-// making user 
-app.put("/user/:email", async (req, res) => {
-    const email = req.params.email
-    const user = req.body
-    const filter = { email: email }
-    const option = { upsert: true }
-    const updateDoc = {
-        $set: user
-    }
-    const result = await usersCollection.updateOne(filter, updateDoc, option)
+        app.get("/tools", async (req, res) => {
+            const result = await toolsCollection.find().toArray()
+            res.send(result)
+        })
+        // making user 
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const filter = { email: email }
+            const option = { upsert: true }
+            const updateDoc = {
+                $set: user
+            }
+            const result = await usersCollection.updateOne(filter, updateDoc, option)
 
-    const token = jwt.sign({ email: email }, process.env.PRIVATE_KEY, { expiresIn: "1d" })
-    res.send({ token })
-})
+            const token = jwt.sign({ email: email }, process.env.PRIVATE_KEY, { expiresIn: "1d" })
+            res.send({ token })
+        })
+        // get single items by id 
+        app.get("/purchase/:id", async(req, res) => {
+            const id = req.params.id
+            
+            const query = { _id: ObjectId(id) }
+            const result=await toolsCollection.findOne(query)
+            res.send(result)
+        })
+        app.post("/purchase", async(req, res) => {
+           const doc=req.body     
+           const result= await ordersCollection.insertOne(doc)
+            res.send(result)
+        })
 
     }
     finally {
